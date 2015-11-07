046d60269eb95a4e92d80046e3385af3a4ca8eb7
mcasimir <maurizio.cas@gmail.com>
2015-11-07 01:20:53 +0100

    completed specs

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 var through     = require('through2'),
     gutil       = require('gulp-util'),
     PluginError = gutil.PluginError,
+    fs          = require('fs'),
     rollup      = require('rollup'),
     PLUGIN_NAME = 'gulp-rollup';
 
@@ -24,25 +25,34 @@ module.exports = function(options) {
 
     if (file.isStream()) {
       return _this.emit('error',
-            new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+        new PluginError(PLUGIN_NAME, 'Streaming not supported'));
     }
 
-    options.entry = file.path;
+    try {
+      var stats = fs.lstatSync(file.relative);
+      if (stats.isFile()) {
+        options.entry = file.path;
 
-    rollup.rollup(options).then(function(bundle){
-      try {
-        var res = bundle.generate(options);
-        file.contents = new Buffer(res.code);
-        _this.push(file);
-        callback();
-      } catch (err) {
-        var ge = new PluginError(PLUGIN_NAME, err.message);
-        _this.emit('error', ge);
+        rollup.rollup(options).then(function(bundle){
+          try {
+            var res = bundle.generate(options);
+            file.contents = new Buffer(res.code);
+            _this.push(file);
+            callback();
+          } catch (err) {
+            var ge = new PluginError(PLUGIN_NAME, err.message);
+            _this.emit('error', ge);
+          }
+        }, function(err){
+          var ge = new PluginError(PLUGIN_NAME, err.message);
+          _this.emit('error', ge);
+        });  
       }
-    }, function(err){
-      var ge = new PluginError(PLUGIN_NAME, err.message);
-      _this.emit('error', ge);
-    });
+    }
+    catch (err) {
+      return _this.emit('error',
+        new PluginError(PLUGIN_NAME, err.message));
+    }
   }, function(){
     this.emit('end');
   });
